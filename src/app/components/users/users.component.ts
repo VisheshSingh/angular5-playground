cb2f0439ff15fdfa9fb69d6ae0f655a53d23ecd0
Vishesh <visheshsingh16@gmail.com>
2018-05-12 16:21:46 -0500

    ngIf conditionals

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -8,52 +8,61 @@ import { User } from "../../models/User";
 })
 export class UsersComponent implements OnInit {
   users: User[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
+
   constructor() {}
 
   ngOnInit() {
-    this.users = [
-      {
-        firstName: "John",
-        lastName: "Doe",
-        age: 30,
-        address: {
-          street: "50th Main Street",
-          city: "Boston",
-          state: "MA"
+    setTimeout(() => {
+      this.users = [
+        {
+          firstName: "John",
+          lastName: "Doe",
+          age: 30,
+          address: {
+            street: "50th Main Street",
+            city: "Boston",
+            state: "MA"
+          }
+        },
+        {
+          firstName: "Kayla",
+          lastName: "Johnson",
+          age: 25,
+          address: {
+            street: "12th Harold Street",
+            city: "Brighton",
+            state: "OK"
+          }
+        },
+        {
+          firstName: "Henry",
+          lastName: "Admas",
+          age: 28,
+          address: {
+            street: "6th Broadway Street",
+            city: "New York",
+            state: "NY"
+          }
         }
-      },
-      {
-        firstName: "Kayla",
-        lastName: "Johnson",
-        age: 25,
-        address: {
-          street: "12th Harold Street",
-          city: "Brighton",
-          state: "OK"
-        }
-      },
-      {
-        firstName: "Henry",
-        lastName: "Admas",
-        age: 28,
-        address: {
-          street: "6th Broadway Street",
-          city: "New York",
-          state: "NY"
-        }
-      }
-    ];
+      ];
 
-    this.addUser({
-      firstName: "Mira",
-      lastName: "Kalyan",
-      age: 22,
-      address: {
-        street: "Harison Street",
-        city: "Princeton",
-        state: "NJ"
-      }
-    });
+      this.loaded = true;
+    }, 2000);
+
+    this.showExtended = false;
+
+    // this.addUser({
+    //   firstName: "Mira",
+    //   lastName: "Kalyan",
+    //   age: 22,
+    //   address: {
+    //     street: "Harison Street",
+    //     city: "Princeton",
+    //     state: "NJ"
+    //   }
+    // });
   }
 
   addUser(user) {
