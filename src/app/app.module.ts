c7ea13cc5839e84296c0cf166c08a62d89bd4240
Vishesh <visheshsingh16@gmail.com>
2018-05-13 11:23:53 -0500

    Making Http GET request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { UserComponent } from "./components/user/user.component";
-import { UsersComponent } from './components/users/users.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { UsersComponent } from "./components/users/users.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { PostsComponent } from "./components/posts/posts.component";
+import { PostService } from "./services/post.service";
 
 @NgModule({
-  declarations: [AppComponent, UserComponent, UsersComponent, NavbarComponent],
-  imports: [BrowserModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    UserComponent,
+    UsersComponent,
+    NavbarComponent,
+    PostsComponent
+  ],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
